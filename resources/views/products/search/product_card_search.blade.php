4c9e3aeeb9754a36fd87afe5b58f7d0c0fab1838
Anastasia <anastasia.lukina1602@icloud.com>
2023-02-09 06:37:09 +0400

    Cleanup code

--- resources/views/products/search/product_card_search.blade.php
+++ resources/views/products/search/product_card_search.blade.php
@@ -2,8 +2,7 @@
     <div class="card" style="width: 18rem;">
         <img src="{{$product->image}}" class="img-thumbnail" alt="..." style="height: 200px;width: 240px;">
         <div class="card-body">
-            <h5 class="card-title">{{$product->name}}</h5>
-            <p class="card-text">{{$product->details}}</p>
+            <h3 class="card-title">{{$product->name}}</h3>
             <a href="{{route('products.show', ['product' => $product->id])}}"
                class="btn btn-primary"> {{__('Show Product')}}</a>
         </div>
